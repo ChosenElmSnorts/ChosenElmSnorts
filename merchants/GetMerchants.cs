58487f6dc9026b8cb86816c640b322ec2e676908
josheb76 <josheb76@vt.edu>
2016-02-27 11:24:24 -0500

    Added files via upload

---
+++ merchants/GetMerchants.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetMerchants : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(getRequest());
+    }
+
+    IEnumerator getRequest()
+    {
+        string url = "http://thebrandmans.net/joshtest/merchants/merchant_ids.html";
+        WWW www = new WWW(url);
+        yield return www;
+
+        GetComponent<TextMesh>().text = www.text;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
